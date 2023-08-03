d7c0a217b60b38fb08b1e390608cab81c097e808
zhangjingyuan <jingyuan.zhang@shopcider.com>
2023-08-03 11:39:59 +0800

    fix bundle issue

--- Sources/Bundle+Extensions.swift
+++ Sources/Bundle+Extensions.swift
@@ -11,10 +11,6 @@ extension Bundle {
 
     /// Returns a package manager appropriate `Bundle`.
     static var resolvedBundle: Bundle {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
         return Bundle(for: LifetimeTracker.self)
-        #endif
     }
 }
